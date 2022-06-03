c8a1c21de4c976c1a6b7e58ed289289ad01ac7bf
Gustavo Tavares <68558112+gsTavares@users.noreply.github.com>
2022-06-02 21:58:39 -0300

    Continuing Class 10 - Destructuring Objects and Spreading

--- day-1/js/destructuring.js
+++ day-1/js/destructuring.js
@@ -53,10 +53,130 @@ const countries = [
     ['norway', 'oslo']
 ];
 
-for(const [country, city] of countries){
+for (const [country, city] of countries) {
     console.log(country, city);
 }
 
 const [x, y] = [2, (value) => value ** 2];
 
 console.log(x, y(2));
+
+/**
+ * Destructuring em objetos
+ */
+
+let rectangle = {
+    width: 20,
+    height: 10
+}
+
+let { width, height } = rectangle;
+
+console.log(width, height) // 20 10
+
+/**
+ * Utilizando alias (renomeando as variáveis)
+ */
+
+let { width: w, height: h } = rectangle;
+console.log(w, h);
+
+const props = {
+    post: {
+        title: 'Destructuring and Spread',
+        lastName: 'Tavares',
+        age: 250
+    }
+}
+
+let { post } = props;
+
+let { title, lastName, age } = post;
+
+console.log(title, lastName, age);
+
+/**
+ * Utilização laço de repetição
+ */
+
+const languages = [
+    { lang: 'Portugueses', count: 91 },
+    { lang: 'English', count: 135 },
+    { lang: 'Spanish', count: 35 }
+];
+
+for (const { lang, count } of languages) {
+    console.log(`The ${lang} is spoken in ${count} countries`);
+}
+
+/**
+ * Destructuring como parâmetro de funções
+ */
+
+rectangle = {
+    width: 20,
+    height: 10
+}
+
+const calculateArea = ({ width: largura, height: altura }) => {
+    return largura * altura;
+}
+
+console.log(calculateArea(rectangle));
+
+/**
+ * Spreading (..rest)
+ * 
+ * Utilizando para copiar arrays e objetos ou recuperar o resto de um array
+ * ou indicar que uma função recebe infinitos parâmetros em uma arrow function
+ * 
+ */
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+[n1, n2, n3, ...otherNums] = numbers;
+
+console.log(...otherNums) // 4,5,6
+
+/**
+ * Copiando um array
+ */
+
+let newNumbers = [...numbers];
+
+console.log(newNumbers) // [1,2,3,4,5,6]
+
+/**
+ * Copiando objetos
+ */
+
+let person = {
+    name: 'Gustavo',
+    title: 'React dev',
+    country: 'Brazil',
+    city: 'Fernandopolis'
+}
+
+let copiedPerson = { ...person }
+console.log(copiedPerson);
+
+/**
+ * Modificando um objeto durante a cópia
+ */
+
+copiedPerson = { ...person, title: 'Java dev' }
+console.log(copiedPerson);
+
+/**
+ * Spreading na arrow function (...args)
+ */
+
+const sum = (...args) => {
+    let result = 0;
+    for (const num of args) {
+        result += num;
+    }
+    return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7));
