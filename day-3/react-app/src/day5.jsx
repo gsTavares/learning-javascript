1bf2556ebb44d1f5a0d643b7c5953bbf8440d025
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-06-08 17:57:36 -0300

    day 5 - using props

---
+++ day-3/react-app/src/day5.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import ReactDOM, { createRoot } from 'react-dom/client'
+/**
+ * React props
+ * 
+ * A palavra reservada 'props' do react faz referência as propriedades que um componente pode receber. 
+ * Essas propriedades recebem dados e eles podem ser apresentados ou manipulados dentro de um componente.
+ * 
+ * As props podem ser utilizadas também para a transferência de dados entre componentes
+ */
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.firstName}
+        {props.lastName}
+      </h1>
+      <small>{props.country}</small>
+    </div>
+  );
+}
+
+const Header = (props) => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{props.welcome}</h1>
+      <h2>{props.title}</h2>
+      <h3>{props.subtitle}</h3>
+      <p>
+        {props.firstName} {props.lastName}
+      </p>
+      <small>{props.date}</small>
+    </div>
+  </header>
+)
+
+const Age = (props) => <div>The person age is {props.age} years old. </div>
+
+const Weight = (props) => <p>The weight of the object on Earth is {props.weight} N.</p>
+
+const Status = (props) => {
+  let status = props.status ? 'Old enough to drive' : 'Too youn for driving';
+  return <p>{status}</p>
+}
+
+const App = () => {
+  // Props do tipo string
+  const welcome = 'Welcome to 30 Days Of React'
+  const title = 'Getting Started React'
+  const subtitle = 'JavaScript Library'
+  const firstName = 'Asabeneh'
+  const lastName = 'Yetayeh'
+  const date = 'Oct 4, 2020'
+
+  // Props do tipo number
+  let currentYear = 2020
+  let birthYear = 1820
+  const age = currentYear - birthYear
+  const gravity = 9.81
+  const mass = 75
+
+  // Props do tipo boolean
+  let status = age >= 18;
+
+  return (
+    <div className='app'>
+      <Header
+        welcome={welcome}
+        title={title}
+        subtitle={subtitle}
+        firstName={firstName}
+        lastName={lastName}
+        date={date}
+      />
+      <Age age={age} />
+      <Weight weight={gravity * mass} />
+      <Status status={status} />
+    </div>
+  )
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
